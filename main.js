e8ca4614e2f288ed00dfb5fa503b3244d8672dac
cody mcnutt <CodyMcNutt@codys-MacBook-Air.local>
2015-09-28 22:42:10 -0600

    animated some elements - not working great

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 angular.module('myApp', ["ngRating"])
 angular.module('myApp').controller('homeButton', function ($scope) {
+
     $scope.snows = []
     $scope.ratings	=[1, 2, 3, 4, 5]
     $scope.rating =0
@@ -8,7 +9,31 @@ angular.module('myApp').controller('homeButton', function ($scope) {
     $scope.page1=true
     $scope.page2=false
     $scope.page3=false
-    
+//     $scope.resorts=[ "ARAPAHOE BASIN", "ELDORA" "SKI GRANBY RANCH" "ASPEN HIGHLANDS" "HOWELSEN"
+
+// "SNOWMASS", "ASPEN MOUNTAIN", "LOVELAND", "STEAMBOAT", "BUTTERMILK"
+
+// "MONARCH", 
+
+// "SUNLIGHT", 
+
+// "COPPER MOUNTAIN", 
+
+// "POWDERHORN", 
+
+// "TELLURIDE", 
+
+// "COOPER", 
+
+// "PURGATORY", 
+
+// "WINTER PARK", 
+
+// "CRESTED BUTTE", 
+
+// "SILVERTON",
+
+// "WOLF CREEK" ]
     $scope.homeMenu = function($event){
                     $scope.snows.push({rating: $scope.rating})
     		
@@ -19,7 +44,6 @@ angular.module('myApp').controller('homeButton', function ($scope) {
     $scope.click = function($event){
         $scope.button1 = !$scope.button1
         $scope.button1= true
-        console.log("yes")
 
     }
       $scope.navpage1 = function($event){
@@ -38,11 +62,25 @@ angular.module('myApp').controller('homeButton', function ($scope) {
         $scope.page3 = true 
     }
 
-        
 
     } )
 
 
-    
-  
+function myIP() {
+    if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
+    else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+
+    xmlhttp.open("GET","http://api.hostip.info/get_html.php",false);
+    xmlhttp.send();
+
+    hostipInfo = xmlhttp.responseText.split("\n");
+
+    for (i=0; hostipInfo.length >= i; i++) {
+        ipAddress = hostipInfo[i].split(":");
+        if ( ipAddress[0] == "IP" ) return ipAddress[1];
+    }
+
+    return false; 
+    console.log(false)
+}
    	
