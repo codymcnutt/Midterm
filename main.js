8fb0e321b0e58341c2928ecf6f1e8103157314d8
Student <student@Mac-mini.local>
2015-09-29 12:31:08 -0600

    update

--- main.js
+++ main.js
@@ -1,16 +1,162 @@
 
 angular.module('myApp', ["ngRating"])
 angular.module('myApp').controller('homeButton', function ($scope) {
-    $scope.snows = []
+    $scope.snowss = []
     $scope.ratings	=[1, 2, 3, 4, 5]
     $scope.rating =0
     $scope.button1 =false
     $scope.page1=true
     $scope.page2=false
     $scope.page3=false
-    
+    $scope.resorts=[
+    {
+        name: "Arapahoe Basin",
+        ratings: 0 ,
+        maps: "test of a map",
+        weather: 0
+    },
+
+    {
+        name: "Winter Park",
+        ratings:0,
+        maps: "test of other maps",
+        weather: 0
+    },
+
+
+    {
+        name:"Copper" ,
+        ratings:0 ,
+        maps: "This is maps", 
+        weather: 0
+
+},
+      {
+        name:"Keystone" ,
+        ratings:0 ,
+        maps: "This is maps" ,
+        weather: 0
+    },
+
+    {
+        name:"Vail" ,
+        ratings:0 ,
+        maps: "This is maps", 
+        weather: 0
+    },
+
+    {
+        name:"Steamboat" ,
+        ratings:0 ,
+        maps: "This is maps" ,
+        weather: 0
+    },
+
+    {
+        name:"Cortez" ,
+        ratings:0 ,
+        maps: "This is maps" ,
+        weather: 0
+    },
+
+    {
+        name:"Eldora" ,
+        ratings:0 ,
+        maps: "This is maps" ,
+        weather: 0
+    },
+
+      {
+        name:"Ski Grandby Ranch" ,
+        ratings:0 ,
+        maps: "This is maps" ,
+        weather: 0
+    },
+
+      {
+        name:"Aspen Highlands" ,
+        ratings:0 ,
+        maps: "This is maps" ,
+        weather: 0
+    },
+
+      {
+        name:"Loveland" ,
+        ratings:0 ,
+        maps: "This is maps" ,
+        weather: 0
+    },
+
+      {
+        name:"Buttermilk" ,
+        ratings:0 ,
+        maps: "This is maps" ,
+        weather: 0
+    },
+       {
+        name:"Buttermilk" ,
+        ratings:0 ,
+        maps: "This is maps" ,
+        weather: 0
+    },
+       {
+        name:"Monarch" ,
+        ratings:0 ,
+        maps: "This is maps" ,
+        weather: 0
+    },
+       {
+        name:"Sunlight" ,
+        ratings:0 ,
+        maps: "This is maps" ,
+        weather: 0
+    },
+
+        {
+        name:"Powderhorn" ,
+        ratings:0 ,
+        maps: "This is maps" ,
+        weather: 0
+    },
+           {
+        name:"Telluride" ,
+        ratings:0 ,
+        maps: "This is maps" ,
+        weather: 0
+    },
+           {
+        name:"Crested Butte" ,
+        ratings:0 ,
+        maps: "This is maps" ,
+        weather: 0
+    },
+           {
+        name:"Silverton" ,
+        ratings:0 ,
+        maps: "This is maps" ,
+        weather: 0
+    },
+           {
+        name:"Wolf Creek" ,
+        ratings:0 ,
+        maps: "This is maps" ,
+        weather: 0
+    },
+           {
+        name:"Purgatory" ,
+        ratings:0 ,
+        maps: "This is maps" ,
+        weather: 0
+    }
+    ]
+   
+
+
+    $scope.location = []
+
+
     $scope.homeMenu = function($event){
-                    $scope.snows.push({rating: $scope.rating})
+                 // $scope.snows.push(snows.rating)   
     		
     		}
            
@@ -19,7 +165,6 @@ angular.module('myApp').controller('homeButton', function ($scope) {
     $scope.click = function($event){
         $scope.button1 = !$scope.button1
         $scope.button1= true
-        console.log("yes")
 
     }
       $scope.navpage1 = function($event){
@@ -28,6 +173,7 @@ angular.module('myApp').controller('homeButton', function ($scope) {
         $scope.page3= false
     }
     $scope.navpage2 = function($event){
+        $scope.snowss.push($scope.snowss.rating)
         $scope.page1 = false
         $scope.page2 = true
         $scope.page3= false
@@ -38,7 +184,9 @@ angular.module('myApp').controller('homeButton', function ($scope) {
         $scope.page3 = true 
     }
 
-        
+       $scope.location =function($event){
+
+       } 
 
     } )
 
--- ng-rating.js
+++ ng-rating.js
@@ -25,7 +25,7 @@
             scope.basedOn = 5;
           }
           if (scope.iconClass == null) {
-            scope.iconClass = "wi wi-snowflake-cold wi-2x"
+            scope.iconClass = "fa fa-star fa-2x"
 
 
 
